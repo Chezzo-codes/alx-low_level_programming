0c04f1a5dc180d462aca5d6575d3593c44b4c3b1
Chezlin Drude <chezlindrude23@gmail.com>
2021-06-17 22:01:20 +0200

    fix script task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,16 @@
 */
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
 	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of a char: %zu bytes\n", sizeof(charType));
+	printf("Size of an int: %zu bytes\n", sizeof(intType));
+	printf("Size of a long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of a long long int: %zu bytes\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte\n", sizeof(floatType));
 	return (0);
 }
