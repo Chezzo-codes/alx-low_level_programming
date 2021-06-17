8afeb32323440f7cefc0ad253cee1160a54fabba
Chezlin Drude <chezlindrude23@gmail.com>
2021-06-17 21:19:58 +0200

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - main function
  * @void: No parameters
- * 
+ *
  * Description: Makes use of the puts function to print a string
  * Return: returns 0 if program is succesfull non-zero if unsuccessful.
  */
