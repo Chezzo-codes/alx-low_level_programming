1d1cf4a549c7148f10d319199b8a78ad7cad14fc
Chezlin Drude <chezlindrude23@gmail.com>
2021-06-20 19:23:16 +0200

    100-print comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description:
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int a;
+	int b;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i/10;
+		b = i%10;
+
+		putchar('0' + a);
+		putchar('0'+ b);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
