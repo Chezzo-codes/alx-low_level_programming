50d361379dd3c41b4b11e9c2e663a72b3f046a85
Chezlin Drude <chezlindrude23@gmail.com>
2021-06-22 21:17:04 +0200

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+* function_name - Short description, single line
+*
+* Description: Longer description of the function)?
+* Return: Description of the returned value
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
