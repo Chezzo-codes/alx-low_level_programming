8a642fbdfba8e5dd39a17c8ee205f431d859565a
Chezlin Drude <chezlindrude23@gmail.com>
2021-06-24 18:53:00 +0200

    Updated holberton.h, Added 5-more_numbers.c

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,6 +3,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 int _islower(int c);
 int _isalpha(int c);
 int _isupper(int c);
