dcb1eb46210754e1b72566ce75247fadff6d5041
Chezlin Drude <chezlindrude23@gmail.com>
2021-06-28 17:28:03 +0200

    Updated holberton.h, Fixed 2-strlen.c, Added 3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,10 +9,11 @@
 int _strlen(char *s)
 {
 	int count = 0;
+	
 	while (*s != '\0')
 	{
 		count++;
 		s++;
 	}
-	return count;
+	return (count);
 }
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -24,3 +24,5 @@ void print_times_table(int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
