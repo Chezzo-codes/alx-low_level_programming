fccc553b72cf91579f2da2f0b50c0ac3d6666dc6
Chezlin Drude <chezlindrude23@gmail.com>
2021-06-30 17:17:21 +0200

    Added tasks 0-strcat.c, 1-strncat.c, 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+/**
+* _strncat - Concat 2 strings with limit
+* @dest: dest string
+* @src: src string
+* @n: memory limit for src
+*
+* Return: pointer to dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p = dest;
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+
+	for (j = 0; src[j] != '0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (p);
+}
