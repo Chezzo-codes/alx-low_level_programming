ef8faa64b74f28ea903b3cb311056f48fa84e2bc
Chezlin Drude <chezlindrude23@gmail.com>
2021-07-19 14:56:13 +0200

    Added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - structure of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
