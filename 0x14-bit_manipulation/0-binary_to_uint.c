59c53262ac3de4010a92d9ddbaceff5bd604135b
Chezlin Drude <chezlindrude23@gmail.com>
2021-08-05 13:14:44 +0200

    Fix task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,20 @@
 /**
+ * _strlen - lenght of string
+ * @str: char string
+ * Return:int
+ */
+int _strlen(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
+
+/**
  * binary_to_uint - Converts binary number to unsigned int.
  * @b: string of binary number
  *
@@ -6,19 +22,25 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec_val = 0;
-	int i = 0;
+	unsigned int k = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int len;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i] != '\0')
+	len = _strlen(b);
+
+	for (c = len - 1; c >= 0; c--)
 	{
-		if (b[i] != '1' && b[i] != '0')
+		if (b[c] != '0' && b[c] != '1')
 			return (0);
-		dec_val <<= 1;
-		dec_val += b[i] - '0';
-		i++;
+		if (b[c] == '1')
+		{
+			i += k;
+		}
+		k *= 2;
 	}
-	return (dec_val);
+	return (i);
 }
