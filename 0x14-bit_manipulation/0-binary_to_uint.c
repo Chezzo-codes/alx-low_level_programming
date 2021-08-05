d804242ec51239f89a5cdc1d9d54d148b6607267
Chezlin Drude <chezlindrude23@gmail.com>
2021-08-05 13:29:50 +0200

    FIXED

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _strlen - lenght of string
  * @str: char string
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <stdio>
-#include <stdlib>
+#include <stddef.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
