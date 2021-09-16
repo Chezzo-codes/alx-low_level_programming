55bafd26492403c39a0cc376d2aecbee560a8f3b
Chezlin Drude <chezlindrude23@gmail.com>
2021-09-16 15:07:26 +0200

    fix betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,26 +1,26 @@
 #include "lists.h"
 /**
- * print_dlistint - returns length of  linked list
+ * dlistint_len - returns length of  linked list
  * @h: pointer to linked list
  *
  * Return: Node count
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t count = 0;
-        const dlistint_t *ptr = h;
+	size_t count = 0;
+	const dlistint_t *ptr = h;
 
-        if (ptr == NULL)
-        {
-                return (0);
-        }
-        else
-        {
-                while (ptr != NULL)
-                {
-                        count++;
-                        ptr = ptr->next;
-                }
-        }
-        return (count);
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (ptr != NULL)
+		{
+			count++;
+			ptr = ptr->next;
+		}
+	}
+	return (count);
 }
