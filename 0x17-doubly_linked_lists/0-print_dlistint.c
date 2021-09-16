f4ac9bc097fb10683512c7fe4abad994f0978ee4
Chezlin Drude <chezlindrude23@gmail.com>
2021-09-16 14:39:52 +0200

    Add readme task 1 and header

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_dlistint - counts number of nodes
+ * @h: pointer to linked list
+ *
+ * Return: Node count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+        const dlistint_t *ptr = h;
+
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (ptr != NULL)
+		{
+			printf("%d\n", ptr->n);
+			count++;
+			ptr = ptr->next;
+		}
+	}
+	return (count);
+}
